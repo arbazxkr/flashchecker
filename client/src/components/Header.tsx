2701147f440efd14eaa4ed9257a99812316fffdf
arbazxkr <122046350+arbazxkr@users.noreply.github.com>
2026-02-11 19:36:28 +0530

    Update branding to Titan logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import Image from "next/image";
 import styles from "./Header.module.css";
 
 interface HeaderProps {
@@ -12,32 +11,13 @@ export default function Header({ connected }: HeaderProps) {
             {/* Brand Left */}
             <div className={styles.brand}>
                 <div className={styles.icon}>
-                    <svg width="32" height="32" viewBox="0 0 28 28" fill="none">
-                        <path
-                            d="M14 2L26 8V20L14 26L2 20V8L14 2Z"
-                            stroke="url(#logo-grad)"
-                            strokeWidth="2"
-                            fill="none"
-                        />
-                        <path
-                            d="M14 6L22 10V18L14 22L6 18V10L14 6Z"
-                            fill="url(#logo-grad)"
-                            opacity="0.3"
-                        />
-                        <path
-                            d="M10 14L13 17L19 11"
-                            stroke="url(#logo-grad)"
-                            strokeWidth="2.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        />
-                        <defs>
-                            <linearGradient id="logo-grad" x1="2" y1="2" x2="26" y2="26">
-                                <stop stopColor="#f2d363" />
-                                <stop offset="1" stopColor="#d4a831" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
+                    <Image
+                        src="/titan-symbol-white.png"
+                        alt="Titan"
+                        width={32}
+                        height={32}
+                        priority
+                    />
                 </div>
                 <span className={styles.name}>FlashChecker</span>
             </div>
